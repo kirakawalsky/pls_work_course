4c934d025490642943c13cf80ba31b2b335d11be
kmironov-dn <kmironov@drivenets.com>
2023-02-06 12:00:06 +0200

    Commit me

---
+++ hola_mundo.py
@@ -0,0 +1,7 @@
+print("Hola mundo!", end=" and ")
+print("Hola mundo aqui!", "1", "2", sep = "lalalla")
+print("Hola mundo! тут")
+print("Como estasComo estasComo estasComo estasComo estasComo estas",
+      "Como estasComo estasComo estasComo estasComo estasComo estas", sep ="")
+print("Como estasComo estasComo estasComo estasComo estasComo estaComo estasComo estasComo estasComo estasComo est"
+      "asComo estas123123123")
